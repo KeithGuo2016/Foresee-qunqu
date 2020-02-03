d3e52d58613fd2e141e9533bce3131f05448e70c
keithguo <keithguo@localhost>
2020-02-03 17:00:40 +0800

    init

---
+++ foresee-qunqu-wx-common/target/classes/snippet/Snippet.java
@@ -0,0 +1,16 @@
+package snippet;
+
+public class Snippet {
+	<!-- swagger2 配置 -->
+	        <dependency>
+	            <groupId>io.springfox</groupId>
+	            <artifactId>springfox-swagger2</artifactId>
+	            <version>2.4.0</version>
+	        </dependency>
+	        <dependency>
+	            <groupId>io.springfox</groupId>
+	            <artifactId>springfox-swagger-ui</artifactId>
+	            <version>2.4.0</version>
+	        </dependency>
+}
+
